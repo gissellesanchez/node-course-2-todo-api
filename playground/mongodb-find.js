c7df8caf3f13d3653fde567eecd981db75fcaaad
gissellesanchez <>
2018-11-14 16:30:41 -0600

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
+  if(err){
+    return console.log('Unable to connect to mongo db server');
+  }
+  console.log('Connected to MongoDB Server');
+
+  // db.collection('Todos').find({
+  //   _id:new ObjectID('5bec96a7db140c449b79791f')
+  // }).toArray().then((docs)=>{
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // },(err)=>{
+  //   console.log('Unable to fetch',err);
+  // });
+
+  // db.collection('Todos').find().count().then((count)=>{
+  //   console.log(`Todos count: ${count}`);
+  // },(err)=>{
+  //   console.log('Unable to fetch',err);
+  // });
+
+db.collection('Users').find({
+  name:'Gisselle'
+}).toArray().then((docs)=>{
+  console.log(JSON.stringify(docs,undefined,2));
+},(err)=>{
+  console.log("Error",err);
+});
+
+
+
+  //db.close();
+});
