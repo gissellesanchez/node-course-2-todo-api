86598b297502428d438a3cb8248a69ce181319ad
Gisselle.Sanchez <>
2018-11-16 09:52:42 -0600

    Add post todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,48 @@
+var express = require ('express');
+var bodyParser=require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user')
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  var todo = new Todo({
+    text:req.body.text
+  });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+
+
+//
+// var newUser = new User({
+//   email:'gisel_sp93@hotmail.com'
+// });
+// newUser.save().then((doc)=>{
+//   console.log(JSON.stringify(doc,undefined,2))
+// },(e)=>{
+//   console.log(e);
+// })
+
+
+// var newTodo = new Todo({
+//   text:' Edit this video '
+// });
+//
+// newTodo.save().then((doc)=>{
+//   console.log(JSON.stringify(doc,undefined,2));
+// },(e)=>{
+//   console.log('Unable to save todo')
+// });
