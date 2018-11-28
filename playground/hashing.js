9236702ddacbc977e2beeb129880fb1741487556
Gisselle.Sanchez <>
2018-11-28 09:26:33 -0600

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-  id:10
-};
-var token=jwt.sign(data,"123abc");
-console.log(token);
+var password='123abc!';
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  });
+});
 
-var decoded=jwt.verify(token,'123abc');
-console.log(decoded);
+var hashPassword = '$2a$10$qB.2zOAz3b0qnvv0H.85wOzExCKAn2CiYdRyYI.06poPkirZMF3Q.';
+bcrypt.compare('123', hashPassword,(err,res)=>{
+  console.log(res);
+});
+
+
+// var data={
+//   id:10
+// };
+// var token=jwt.sign(data,"123abc");
+// console.log(token);
+//
+// var decoded=jwt.verify(token,'123abc');
+// console.log(decoded);
 // jwt.verify
 
 
--- server/server.js
+++ server/server.js
@@ -101,6 +101,7 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
+  console.log(user);
 
   user.save().then(() => {
     return user.generateAuthToken();
