97c30f383ed3928a8d824a3729a918528e88d731
Gisselle.Sanchez <>
2018-11-28 16:12:46 -0600

    Associate users with todos

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
-
 var Todo=mongoose.model('Todo',{
   text:{
     type:String,
@@ -15,7 +14,12 @@ var Todo=mongoose.model('Todo',{
   completedAt:{
     type:Number,
     default:null
+  },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
   }
+
 });
 
 module.exports = {Todo};
